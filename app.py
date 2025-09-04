8a0d5bd141d2575e2ffb4969cceaef8016a138f5
Muzammil Hussain <121623043+muzammil-15@users.noreply.github.com>
2025-09-04 22:34:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from fastapi import FastAPI
-import joblib
-import uvicorn
-from pydantic import BaseModel
+# from fastapi import FastAPI
+# import joblib
+# import uvicorn
+# from pydantic import BaseModel
 
-app = FastAPI()
+# app = FastAPI()
 
-# Load trained model
-model = joblib.load("url_model_tldfreq.pkl")
+# # Load trained model
+# model = joblib.load("url_model_tldfreq.pkl")
 
-@app.get("/")
-def home():
-    return {"message": "Malicious URL Detection API is running"}
+# @app.get("/")
+# def home():
+#     return {"message": "Malicious URL Detection API is running"}
 
 
-class URLItem(BaseModel):
-    url: str
+# class URLItem(BaseModel):
+#     url: str
 
-@app.post("/predict")
-def predict_url(item: URLItem):
-    prediction = model.predict([item.url])[0]
-    return {"url": item.url, "malicious": bool(prediction)}
+# @app.post("/predict")
+# def predict_url(item: URLItem):
+#     prediction = model.predict([item.url])[0]
+#     return {"url": item.url, "malicious": bool(prediction)}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
